82394dc10fbd5065c4b1b0e94ddbdba673c459c5
Seongbo <hsb9504@gmail.com>
2023-01-27 23:21:11 +0900

    ViewPager를 활용한 초기 화면 구성

--- presentation/src/main/java/com/project/presentation/activity/MainActivity.kt
+++ presentation/src/main/java/com/project/presentation/activity/MainActivity.kt
@@ -3,7 +3,12 @@ package com.project.presentation.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.widget.ViewPager2
+import com.project.presentation.R
 import com.project.presentation.databinding.ActivityMainBinding
+import com.project.presentation.fragment.MyPageFragment
+import com.project.presentation.fragment.MainFragment
+import com.project.presentation.fragment.ViewPagerAdapter
 import com.project.presentation.viewmodel.BmiViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,5 +25,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+        binding.viewPager.isUserInputEnabled = false
+
+        binding.viewPager.registerOnPageChangeCallback(
+            object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    binding.bottomMenu.menu.getItem(position).isChecked = true
+                }
+            }
+        )
+
+        binding.bottomMenu.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.main -> {
+                    binding.viewPager.currentItem = 0
+                    true
+                }
+                else -> {
+                    binding.viewPager.currentItem = 1
+                    true
+                }
+            }
+        }
     }
 }
